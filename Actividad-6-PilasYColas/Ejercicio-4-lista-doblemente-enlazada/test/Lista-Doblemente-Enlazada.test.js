aa1f7bd087426ea34f927e2a48cb077cc7739b37
DanSaurio <a22311168@uthermosillo.edu.mx>
2023-11-30 16:26:24 -0700

    Actividad-6-PilasYColasFinalizado

---
+++ Actividad-6-PilasYColas/Ejercicio-4-lista-doblemente-enlazada/test/Lista-Doblemente-Enlazada.test.js
@@ -0,0 +1,37 @@
+const Stack = require('../Lista-Doblemente-Enlazada');
+
+describe('Stack', () => {
+  it('should initialize an empty stack', () => {
+    const stack = new Stack();
+    expect(stack.size()).toBe(0);
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  it('should push elements onto the stack', () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    expect(stack.size()).toBe(3);
+    expect(stack.isEmpty()).toBe(false);
+    expect(stack.peek()).toBe(3);
+  });
+
+  it('should pop elements from the stack', () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    expect(stack.pop()).toBe(3);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+    expect(stack.size()).toBe(0);
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  it('should handle peek and pop on an empty stack', () => {
+    const stack = new Stack();
+    expect(stack.peek()).toBeUndefined();
+    expect(stack.pop()).toBeUndefined();
+  });
+});
